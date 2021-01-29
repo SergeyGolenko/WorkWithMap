11bcb020cdf3aa3b06d6f6d0f03e68f62f7ab5d1
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-29 13:20:57 +0200

    Initial Commit

---
+++ WorkWithMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkWithMap
+//
+//  Created by Сергей Голенко on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

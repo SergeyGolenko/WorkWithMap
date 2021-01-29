93cc14011767655135c4117abbcc4bb5b1e96de2
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-29 18:44:28 +0200

    добавляются кастомные аннотации

---
+++ WorkWithMap/CoffeeAnnotation.swift
@@ -0,0 +1,13 @@
+//
+//  CoffeeAnnotation.swift
+//  WorkWithMap
+//
+//  Created by Сергей Голенко on 29.01.2021.
+//
+
+import MapKit
+
+class CoffeeAnnotation : MKPointAnnotation {
+    
+    var imageURL : String!
+}
--- WorkWithMap/ViewController.swift
+++ WorkWithMap/ViewController.swift
@@ -15,7 +15,19 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
     @IBOutlet weak var pageControllMap: UISegmentedControl!
     @IBOutlet weak var mapView : MKMapView!
     private let locationManager = CLLocationManager()
-   
+    
+    private var userCoordinateForNewAnnotation = CLLocationCoordinate2D()
+    
+    
+    @IBAction func addAnnotationButton(_ sender: UIButton) {
+        
+        let newCoordinate = userCoordinateForNewAnnotation
+        let myAnnotation = CoffeeAnnotation()
+        myAnnotation.imageURL = "pointCoffee"
+        myAnnotation.coordinate = userCoordinateForNewAnnotation
+        self.mapView.addAnnotation(myAnnotation)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +44,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
     }
     
     @objc func mapTypeChanged(segmentControl:UISegmentedControl){
-        switch(segmentControl.selectedSegmentIndex){
+        switch segmentControl.selectedSegmentIndex{
         case 0 : mapView.mapType = .standard
         case 1 : mapView.mapType = .satellite
         case 2 : mapView.mapType = .hybrid
@@ -51,10 +63,55 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
     //Привязывает вид карты к положению userLocation 
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         let coordinate2D = userLocation.coordinate
+        userCoordinateForNewAnnotation = coordinate2D
         //coordinateSpan увеличивает местоположение
         let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
         var region = MKCoordinateRegion(center: coordinate2D, span: coordinateSpan)
         mapView.setRegion(region, animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation{
+            
+            var userView = mapView.dequeueReusableAnnotationView(withIdentifier: "car")
+            if userView == nil {
+                userView = MKAnnotationView(annotation: annotation, reuseIdentifier: "car")
+                userView?.canShowCallout = true
+            } else {
+                userView?.annotation = annotation
+            }
+            userView?.image = UIImage(named: "car")
+            return userView
+        }
+        
+        
+        var coffeeAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "CoffeeAnnotationView")
+        
+        if coffeeAnnotationView == nil {
+            coffeeAnnotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "CoffeeAnnotationView")
+            coffeeAnnotationView?.canShowCallout = true
+        } else {
+            coffeeAnnotationView?.annotation = annotation
+        }
+        if let coffeeAnnotation = annotation as? CoffeeAnnotation {
+            coffeeAnnotationView?.image = UIImage(named: coffeeAnnotation.imageURL)
+        }
+        configureView(coffeeAnnotationView)
+        return coffeeAnnotationView
+    }
+    
+    private func configureView(_ annotationView :MKAnnotationView?) {
+        let view = UIView(frame: CGRect.zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        view.backgroundColor = UIColor.red
+        
+        annotationView?.leftCalloutAccessoryView = UIImageView(image: UIImage(named :"pointCoffee"))
+        annotationView?.rightCalloutAccessoryView = UIImageView(image: UIImage(named :"car"))
+        annotationView?.detailCalloutAccessoryView = view
+        
     }
 
 }
